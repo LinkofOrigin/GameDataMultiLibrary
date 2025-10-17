8ad5de30260a4e3daa8322a429cae1a02a0a4d46
Link of Origin <jesselink333@gmail.com>
2025-10-17 15:26:08 -0600

    v1 - Now pulling from IGDB API

--- site/public/gameData/gameData.js
+++ site/public/gameData/gameData.js
@@ -1,3 +1,42 @@
+// Slugs
+const PRIMARY_DATA = "primaryData";
+const SECONDARY_DATA = "secondaryData";
+const TERTIARY_DATA = "tertiaryData";
+
+
+function injectDataToPage(sectionSlug, data) {
+    // Locate the housing element for this data
+    const dataContainerSlug = sectionSlug + "Container";
+    let dataContainerElement = document.getElementById(dataContainerSlug);
+    
+    const dataTitle = data.title.slice(0, 1).toUpperCase() + data.title.slice(1).toLowerCase();
+    dataContainerElement.querySelector("[name='title']").innerHTML = dataTitle;
+    
+    const dataSummary = data.summary;
+    dataContainerElement.querySelector("[name='summary']").innerHTML = dataSummary;
+    
+    const rawData = JSON.stringify(data.data, null, 6);
+    dataContainerElement.querySelector("[name='data']").innerText = rawData;
+}
+
+/**
+ * Convert the API data format to a consistent structure for the client to process.
+ * 
+ * @param {JSON} apiData 
+ * @returns 
+ */
+function parseData(apiData) {
+    const primaryData = apiData.primary;
+    const secondaryData = apiData.secondary;
+    const tertiaryData = apiData.tertiary;
+    
+    return {
+        primary: primaryData,
+        secondary: secondaryData,
+        tertiary: tertiaryData
+    }
+}
+
 
 export default async function getGameData() {
     let gameDataResponse = await fetch("/get_idgb_data");
@@ -6,8 +45,14 @@ export default async function getGameData() {
         throw new Error("Failed to retrieve game data!");
     }
     
-    let gameData = await gameDataResponse.text();
-    console.log(`Client received game data: ${gameData}`);
+    const gameData = await gameDataResponse.json();
+    console.log(`Client received game data: ${JSON.stringify(gameData)}`);
+    
+    const parsedData = parseData(gameData);
+    
+    injectDataToPage(PRIMARY_DATA, parsedData.primary);
+    injectDataToPage(SECONDARY_DATA, parsedData.secondary);
+    injectDataToPage(TERTIARY_DATA, parsedData.tertiary);
     
     return gameData;
 }
--- site/src/igdb_api/request_data.js
+++ site/src/igdb_api/request_data.js
@@ -1,24 +1,64 @@
 import IGDB_API from "./igdb_api";
 import { ensureOAuthToken } from "../twitch_oauth/request_token";
 
-async function getGenreData(igdb) {
+// Get a list of Genres registered with IGDB
+async function getPrimaryData(igdb) {
     let endpoint = "genres";
-    let fields = ["name", "slug", "updated_at"];
+    let fields = [
+        "name",
+        "updated_at",
+    ];
     let filters = {
         limit: 5,
     };
     
     let genreResponse = await igdb.request(endpoint, fields, filters);
-    // console.log("Received genre data from IGDB: " + genreResponse);
+    console.log("Retrieved primary data from IGDB.");
     return genreResponse;
 }
 
-async function getSecondaryGamesData(igdb) {
-    return "NULL";
+// Get Games from IGDB with few but high ratings (gems)
+async function getSecondaryData(igdb) {
+    let endpoint = "games";
+    let fields = [
+        "name",
+        "summary",
+        "aggregated_rating",
+        "aggregated_rating_count",
+        "updated_at",
+    ];
+    let filters = {
+        where: "aggregated_rating > 70 & aggregated_rating_count <= 100 & aggregated_rating_count > 10",
+        limit: 5,
+    };
+    
+    let genreResponse = await igdb.request(endpoint, fields, filters);
+    console.log("Retrieved secondary data from IGDB.");
+    return genreResponse;
 }
 
+// Get 5 random characters (alphabetized)
 async function getTertiaryData(igdb) {
-    return "NULL";
+    let endpoint = "characters";
+    let fields = [
+        "name",
+        "games.name",
+        "updated_at",
+        "description",
+    ];
+    
+    const randomPageNum = Math.floor(Math.random() * 1000); // 1000 is arbitrary, no idea how many total pages their will be
+    
+    let filters = {
+        sort: "name asc",
+        where: "games != null",
+        limit: 5,
+        offset: randomPageNum,
+    };
+    
+    let genreResponse = await igdb.request(endpoint, fields, filters);
+    console.log("Retrieved tertiary data from IGDB.");
+    return genreResponse;
 }
 
 export default async function requestGameData(request, env, ctx) {
@@ -29,14 +69,27 @@ export default async function requestGameData(request, env, ctx) {
     let accessToken = await env.IGDB.get("ACCESS_TOKEN");
     let igdbApi = new IGDB_API(clientId, accessToken);
     
-    let genreData = await getGenreData(igdbApi);
-    let secondaryData = await getSecondaryGamesData(igdbApi);
+    let primaryData = await getPrimaryData(igdbApi);
+    let secondaryData = await getSecondaryData(igdbApi);
     let tertiaryData = await getTertiaryData(igdbApi);
     console.log("Retrieved various data from IGDB.");
     
+    // Wrap up our return data into some objects with notable fields
     return {
-        "genres": genreData,
-        "secondary": secondaryData,
-        "tertiary": tertiaryData
+        "primary": {
+            "title": "Genres",
+            "summary": "5 genres the API returns (no order or sort logic).",
+            "data": primaryData,
+        },
+        "secondary": {
+            "title": "Games",
+            "summary": "5 games with a high average rating but relatively few total ratings. Maybe your next favorite game is here?",
+            "data": secondaryData,
+        },
+        "tertiary": {
+            "title": "Characters",
+            "summary": "5 characters with at least one associated game. These characters were selected by sorting their names alphabetically and then pulling a random page number.",
+            "data": tertiaryData,
+        },
     }
 }
